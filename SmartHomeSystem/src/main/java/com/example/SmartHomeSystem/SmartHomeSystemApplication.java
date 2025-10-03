3d6259d13194c5d809fcd3eab88a14f13ef5f8fa
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2025-10-03 14:19:37 +0300

    Rebuild project
    Create new repository and new project without trash from old version

---
+++ SmartHomeSystem/src/main/java/com/example/SmartHomeSystem/SmartHomeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartHomeSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeSystemApplication.class, args);
+	}
+
+}
