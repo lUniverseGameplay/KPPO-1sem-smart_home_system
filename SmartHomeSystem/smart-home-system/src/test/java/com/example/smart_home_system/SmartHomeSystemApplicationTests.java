88c2c984e8948dbfc415a728c47251e5c903b27b
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2025-11-01 09:25:58 +0300

    add base project to branch

---
+++ SmartHomeSystem/smart-home-system/src/test/java/com/example/smart_home_system/SmartHomeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_home_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
