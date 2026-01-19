09d73c5fc3685ffa5c5134ab3d348784f0156337
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2026-01-19 19:51:33 +0300

    Fix login error

--- SmartHomeSystem/smart-home-syst/src/main/java/com/example/smart_home_syst/model/User.java
+++ SmartHomeSystem/smart-home-syst/src/main/java/com/example/smart_home_syst/model/User.java
@@ -17,11 +17,14 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+//@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="users")
